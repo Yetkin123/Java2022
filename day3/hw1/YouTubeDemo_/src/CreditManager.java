6af8f17d25d5c6730c19603d3eef0a5717f8187a
Yetkin <aayeren@gmail.com>
2022-10-18 21:41:33 +0200

    Add files via upload

---
+++ day3/hw1/YouTubeDemo_/src/CreditManager.java
@@ -0,0 +1,15 @@
+public class CreditManager {
+    public void calculate(int creditType) {
+        // SonarQube
+        if (creditType == 1) { // esnaf
+            // kod
+        }
+        if (creditType == 2) { // öğretmen
+            // kod
+        }
+        System.out.println("Hesaplandı");
+    }
+    public void save() {
+        System.out.println("Kredi verildi");
+    }
+}
---
+++ day3/hw1/YouTubeDemo_/src/TeacherCreditManager.java
@@ -0,0 +1,12 @@
+public class TeacherCreditManager extends BaseCreditManager implements ICreditManager {
+    @Override
+    public void calculate() {
+        // hesaplamamlar
+        System.out.println("Öğretmen kredisi hesaplandı");
+    }
+    @Override
+    public void save() {
+        // hesaplamamlar
+        System.out.println("Öğretmen kredisi hesaplandı");
+    }
+}
